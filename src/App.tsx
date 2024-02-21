073c3d66bf1fce259db329073a818f7b3f945182
Galina Lasko <galina_lasko@hotmail.com>
2024-02-21 22:57:30 +0100

    transfer my files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Lesson20 from "./lessons/Lesson20/lesson20";
+
+// Lessons
+
+
+function App() {
+  return (
+    <div className="App">
+      <Lesson20 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/lessons/Lesson20/Lesson20.tsx
@@ -0,0 +1,21 @@
+import "./App.css"
+function Lesson20() {
+  let someString: string="This is string";
+  console.log(someString);
+  someString="This is change string";
+  console.log(someString);
+  // someString=5;
+  console.log(someString);
+  // let someString2: string=10;
+  let somenumber: number=30;
+  let numberWithDot: number=110.375;
+  let isActive: boolean=true;
+  isActive=false;
+  // isActive=undefined;
+  const animals: string[]=["cat","dog"];
+  // animals.push(1000);
+
+    return <div>Lesson 20</div>;
+  }
+  
+  export default Lesson20;
