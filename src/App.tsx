81abbb280df36834345ba1e65c4b94a4d826327b
Galina Lasko <galina_lasko@hotmail.com>
2024-02-29 21:26:16 +0100

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 
 // import Homework20 from "./homeworks/Homework20/Homework20";
-
+import GlobalStyles from "styles/GlobalStyles";
 import Homework22 from "homeworks/Homework22/Homework22";
+import Lesson23 from "lessons/Lesson23/Lesson23";
 
 
 function App() {
-  return (
-    <div className="App">
+  return ( 
+    <>
+      <GlobalStyles/>
       {/* <Lesson20 /> */}
       {/* <Lesson21 /> */}
-      <Homework22 />
+      {/* <Homework22 /> */}
+      <Lesson23/>
       {/* <Homework20 /> */}
       {/* <Homework21 /> */}
-    </div>
+      </>
   );
 }
-
 export default App;
